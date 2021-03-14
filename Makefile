89aeade89919dcdad2b7b8157ef04a2d2f16cee8
Roberto Rivera <roberto.rivera@gmail.com>
2021-03-14 18:04:53 -0500

    Require ykman > 4.0.0. Remove workflow code.

--- Makefile
+++ Makefile
@@ -2,19 +2,19 @@ VERSION:=$(shell cat ./src/version)
 
 all: clean build
 
-build:
+build: requirements
 	cp README.md src/
-	VERSION=$(VERSION) envsubst < src/info.plist.template > src/info.plist 
+	VERSION=$(VERSION) envsubst < src/info.plist.template > src/info.plist
 	cd src ; \
 	zip ../Yubikey-for-Alfred-v$(VERSION).alfredworkflow . -r --exclude=*.DS_Store* --exclude=*.pyc* --exclude=*.pyo* --exclude=*.plist.template*
 	rm src/README.md src/info.plist
 
-release:
+release: build
 	ghr v$(VERSION) Yubikey-for-Alfred-v$(VERSION).alfredworkflow
 
 clean:
 	rm -f *.alfredworkflow
 
-update-lib:
-	pip install --target src --upgrade Alfred-Workflow
+requirements:
+	pip install --target src --upgrade Alfred-Workflow packaging
 	rm -rf src/Alfred_Workflow-*-info/
--- src/yubi.py
+++ src/yubi.py
@@ -4,23 +4,24 @@ import os
 import subprocess
 import sys
 import re
+from packaging import version
 
 from workflow import Workflow, MATCH_SUBSTRING, ICON_ERROR, util
 from workflow.background import run_in_background
 from icons import get_icon_for_service
 
-GITHUB_SLUG = 'robertoriv/alfred-yubikey-otp'
+GITHUB_SLUG = "robertoriv/alfred-yubikey-otp"
+YKMAN_MIN_VERSION = "4.0.0"
 
 log = None
 
 
 def execute(cmd_list):
     new_env = os.environ.copy()
-    new_env['PATH'] = '/usr/local/bin:%s' % new_env['PATH']
-    cmd, err = subprocess.Popen(cmd_list,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                env=new_env).communicate()
+    new_env["PATH"] = "/usr/local/bin:%s" % new_env["PATH"]
+    cmd, err = subprocess.Popen(
+        cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=new_env
+    ).communicate()
     if err:
         return err
     return cmd
@@ -28,14 +29,14 @@ def execute(cmd_list):
 
 def get_all_codes():
     formatted_codes = []
-    codes = execute(['ykman', 'oath', 'accounts', 'code']).splitlines()
+    codes = execute(["ykman", "oath", "accounts", "code"]).splitlines()
     for code in codes:
         log.info(code)
-        code_search = re.search('(.*)((\d{6,8})|(\[Touch))', code, re.IGNORECASE)
+        code_search = re.search("(.*)((\d{6,8})|(\[Touch))", code, re.IGNORECASE)
         if code_search:
             entry = {
                 "name": code_search.group(1).strip(),
-                "code": code_search.group(2).strip()
+                "code": code_search.group(2).strip(),
             }
             formatted_codes.append(entry)
     log.info(formatted_codes)
@@ -43,41 +44,68 @@ def get_all_codes():
 
 
 def key_for_codes(code):
-    return code['name']
+    return code["name"]
 
 
 def filter_available_codes(wf, query):
-    available_codes = wf.cached_data('ykman_available_codes', get_all_codes, max_age=10)
-    if query != None:
+    available_codes = wf.cached_data("ykman_available_codes", get_all_codes, max_age=10)
+    if query is not None:
         query_filter = query.split()
         if query_filter:
-            return wf.filter(query_filter[0], available_codes, key_for_codes, match_on=MATCH_SUBSTRING)
+            return wf.filter(
+                query_filter[0],
+                available_codes,
+                key_for_codes,
+                match_on=MATCH_SUBSTRING,
+            )
     return available_codes
 
 
+def get_ykman_version():
+    log.info("Getting ykman vesion...")
+    v_string = execute(["ykman", "--version"]).splitlines()[0]
+    return re.search(r"(\d+\.\d+\.\d+)$", v_string).group(1)
+
+
+def check_ykman_version():
+    v = wf.cached_data("ykman_version", get_ykman_version, max_age=3600)
+    parsed_version = version.parse(v)
+    log.info("Current ykman version: " + str(parsed_version))
+    if parsed_version < version.parse(YKMAN_MIN_VERSION):
+        log.info("Clearing the 'ykman_version' cache...")
+        wf.cache_data(
+            "ykman_version", None
+        )  # clear the cache to force a read from the cli next time
+        return False
+    return True
+
+
 def yubikey_not_inserted():
-    codes = execute(['ykman', 'oath', 'code']).splitlines()
-    if 'Error: No YubiKey detected!' in codes:
+    codes = execute(["ykman", "oath", "accounts", "code"]).splitlines()
+    if "Error: No YubiKey detected!" in codes:
         return True
     return False
 
 
 def ykman_installed():
-    return os.path.isfile('/usr/local/bin/ykman')
+    return os.path.isfile("/usr/local/bin/ykman")
+
 
 def touch(name):
     new_env = os.environ.copy()
-    new_env['PATH'] = '/usr/local/bin:%s' % new_env['PATH']
-    process = subprocess.Popen(['ykman', 'oath', 'code', name], stdout=subprocess.PIPE, env=new_env)
+    new_env["PATH"] = "/usr/local/bin:%s" % new_env["PATH"]
+    process = subprocess.Popen(
+        ["ykman", "oath", "accounts", "code", name], stdout=subprocess.PIPE, env=new_env
+    )
     counter = 0
-    for i in iter(process.stdout.readline,'b'):
-        code_search = re.search('(.*)((\d{6,8}))', i, re.IGNORECASE)
+    for i in iter(process.stdout.readline, "b"):
+        code_search = re.search(r"(.*)((\d{6,8}))", i, re.IGNORECASE)
         if code_search:
             wf.add_item(
                 name,
-                'Copy %s to clipboard' % code_search.group(2).strip(),
+                "Copy %s to clipboard" % code_search.group(2).strip(),
                 arg=code_search.group(2).strip(),
-                icon=get_icon_for_service(name), 
+                icon=get_icon_for_service(name),
                 valid=True,
             )
             wf.send_feedback()
@@ -86,50 +114,68 @@ def touch(name):
         if counter == 3:
             wf.add_item(
                 name,
-                'Timeout',
+                "Timeout",
                 arg=name,
-                icon=get_icon_for_service(name), 
+                icon=get_icon_for_service(name),
                 valid=True,
             )
             wf.send_feedback()
             break
     process.kill()
 
+
 def main(wf):
 
     if not ykman_installed():
-        wf.add_item('Error', 'Please install ykman (brew install ykman)...', icon=ICON_ERROR, valid=True)
+        wf.add_item(
+            "Error",
+            "Please install ykman (brew install ykman)...",
+            icon=ICON_ERROR,
+            valid=True,
+        )
+    elif not check_ykman_version():
+        wf.add_item(
+            "Error",
+            "Please upgrade to ykman >= 4.0.0 (brew upgade ykman)...",
+            icon=ICON_ERROR,
+            valid=True,
+        )
     elif yubikey_not_inserted():
-        wf.add_item('Error', 'Please insert your Yubikey...', icon=ICON_ERROR, valid=True,)
+        wf.add_item(
+            "Error",
+            "Please insert your Yubikey...",
+            icon=ICON_ERROR,
+            valid=True,
+        )
     else:
         # extract query
         query = wf.args[0] if len(wf.args) else None
         codes = filter_available_codes(wf, query)
         for code in codes:
-            if code['code'] == '[Touch':
+            if code["code"] == "[Touch":
                 if len(codes) == 1:
-                    touch(code['name'])
+                    touch(code["name"])
                 else:
                     wf.add_item(
-                        code['name'], 
-                        'Require Touch', 
-                        arg=code['name'], 
-                        icon=get_icon_for_service(code['name']), 
+                        code["name"],
+                        "Require Touch",
+                        arg=code["name"],
+                        icon=get_icon_for_service(code["name"]),
                         valid=True,
                     )
             else:
                 wf.add_item(
-                code['name'], 
-                'Copy to %s clipboard...' % code['code'], 
-                arg=code['code'], 
-                icon=get_icon_for_service(code['name']), 
-                valid=True,
-            )
+                    code["name"],
+                    "Copy to %s clipboard..." % code["code"],
+                    arg=code["code"],
+                    icon=get_icon_for_service(code["name"]),
+                    valid=True,
+                )
 
     wf.send_feedback()
 
-if __name__ == '__main__':
-    wf = Workflow(update_settings={'github_slug': GITHUB_SLUG})
+
+if __name__ == "__main__":
+    wf = Workflow(update_settings={"github_slug": GITHUB_SLUG})
     log = wf.logger
     sys.exit(wf.run(main))
-
